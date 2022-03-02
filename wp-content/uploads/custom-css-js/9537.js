beac9d9fe605898ad4341b144abe48962a1fcb02
apimatic <developer@apimatic.io>
2022-03-02 12:08:08 +0500

    [skip netlify]Added simply-static-1-1646204247

---
+++ wp-content/uploads/custom-css-js/9537.js
@@ -0,0 +1,10 @@
+<!-- start Simple Custom CSS and JS -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-7PG6FKNYH6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-7PG6FKNYH6');
+</script><!-- end Simple Custom CSS and JS -->
