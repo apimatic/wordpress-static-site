b02316321d1af6d4a0c1eb2e2bd0319fa1d3744e
apimatic <developer@apimatic.io>
2022-03-02 12:08:21 +0500

    [skip netlify]Added simply-static-1-1646204247

---
+++ wp-content/uploads/custom-css-js/9530.js
@@ -0,0 +1,13 @@
+<!-- start Simple Custom CSS and JS -->
+<!-- Start of apimatic Zendesk Widget script -->
+  <script type="text/javascript">
+  window.zESettings = {
+    webWidget: {
+      chat: {
+        connectOnPageLoad: false
+      }
+    }
+  };
+</script>
+<script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=bbbf6b17-7451-43eb-8743-7a505cf9fa3a"> </script>
+<!-- End of apimatic Zendesk Widget script --><!-- end Simple Custom CSS and JS -->
