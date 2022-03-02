e24e48d404533d7390ab17ca48a7b70c4fb790af
apimatic <developer@apimatic.io>
2022-03-02 12:08:17 +0500

    [skip netlify]Added simply-static-1-1646204247

---
+++ wp-content/uploads/custom-css-js/5972.js
@@ -0,0 +1,7 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})</script>
+<!-- end Simple Custom CSS and JS -->
